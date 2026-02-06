35ed7873d5ee323fc9e3039e845e037bcce00be8
Neeraj Kumar <neeku@eg.dk>
2026-02-06 12:25:02 +0530

    Navigation in progress

--- App.tsx
+++ App.tsx
@@ -13,12 +13,13 @@ import {
 } from 'react-native-safe-area-context';
 // import CommUI from './components/CommUI';
 // import ItemList from './components/ItemList'
-import LinearGradient from 'react-native-linear-gradient';
-import { useState } from 'react';
-import OpponentScreen from './components/guessGame/OpponentScreen';
-import GameStartScreen from './components/guessGame/GameStartScreen';
-import Colors from './constants/colors';
-import GameOverScreen from './components/guessGame/GameOverScreen';
+
+// import LinearGradient from 'react-native-linear-gradient';
+// import { useState } from 'react';
+// import OpponentScreen from './components/guessGame/OpponentScreen';
+// import GameStartScreen from './components/guessGame/GameStartScreen';
+// import Colors from './constants/colors';
+// import GameOverScreen from './components/guessGame/GameOverScreen';
 
 type StatusProp = { trails: number, status: boolean };
 function App() {
@@ -32,40 +33,40 @@ function App() {
     }
   });
 
-  const [userNumber, setUserNumber] = useState(NaN);
-  const [gameIsOver, setGameIsOver] = useState(false);
-  const [trialStatus, setTrailStatus] = useState<StatusProp>({ trails: 0, status: false });
+  // const [userNumber, setUserNumber] = useState(NaN);
+  // const [gameIsOver, setGameIsOver] = useState(false);
+  // const [trialStatus, setTrailStatus] = useState<StatusProp>({ trails: 0, status: false });
 
-  const pickedNumberHandler = (pickedNumber: number) => {
-    setUserNumber(pickedNumber);
-  }
+  // const pickedNumberHandler = (pickedNumber: number) => {
+  //   setUserNumber(pickedNumber);
+  // }
 
-  const gameOverHandler = (newTrailStatus: StatusProp) => {
-    setGameIsOver(true);
-    setTrailStatus(newTrailStatus);
-  } 
+  // const gameOverHandler = (newTrailStatus: StatusProp) => {
+  //   setGameIsOver(true);
+  //   setTrailStatus(newTrailStatus);
+  // } 
 
-  const startNewGameHandler = ()=>{
-    setUserNumber(NaN);
-    setGameIsOver(false);
-  }
+  // const startNewGameHandler = ()=>{
+  //   setUserNumber(NaN);
+  //   setGameIsOver(false);
+  // }
 
-  let screen = <GameStartScreen onPickNumber={pickedNumberHandler} />
-  if (!Number.isNaN(userNumber)) {
-    screen = (
-      <OpponentScreen userNumber={userNumber} onGameOver={gameOverHandler} />
-    );
-  }
-  if (gameIsOver && trialStatus.status) {
-    screen = <GameOverScreen trailStatus={trialStatus} userNumber={userNumber} onStartNewGame={startNewGameHandler}/>
-  }
+  // let screen = <GameStartScreen onPickNumber={pickedNumberHandler} />
+  // if (!Number.isNaN(userNumber)) {
+  //   screen = (
+  //     <OpponentScreen userNumber={userNumber} onGameOver={gameOverHandler} />
+  //   );
+  // }
+  // if (gameIsOver && trialStatus.status) {
+  //   screen = <GameOverScreen trailStatus={trialStatus} userNumber={userNumber} onStartNewGame={startNewGameHandler}/>
+  // }
 
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       {/* <CommUI /> */}
       {/* <ItemList /> */}
-      <LinearGradient colors={[Colors.primary600, Colors.accent500]} style={styles.rootStyle}>
+      {/* <LinearGradient colors={[Colors.primary600, Colors.accent500]} style={styles.rootStyle}>
         <ImageBackground source={require('./assets/images/background.png')}
           resizeMode='cover' style={styles.rootStyle} imageStyle={styles.bgImgStyle}>
           <SafeAreaView>
@@ -74,7 +75,7 @@ function App() {
             </ScrollView>
           </SafeAreaView>
         </ImageBackground>
-      </LinearGradient>
+      </LinearGradient> */}
     </SafeAreaProvider>
   );
 }
