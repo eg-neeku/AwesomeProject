9f08afd4c1bbcd12e2d360a0eda1bc56358b941f
Neeraj Kumar <neeku@eg.dk>
2026-03-06 10:41:38 +0530

    College Project Part1: Basic GUI refined

---
+++ src/components/college/database/model.tsx
@@ -0,0 +1,16 @@
+
+export type DetailsProp = {
+    id: string,
+    name: string,
+}
+
+export const BUILDING_DATA: DetailsProp[] = [
+    {
+        id: "a",
+        name: "BuildingA"
+    },
+    {
+        id: "b",
+        name: "BuildingB"
+    }
+];
